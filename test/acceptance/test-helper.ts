3af57e1ee20a8e3a064cabb5d0ac99bff6ffe57f
Paul <prasenjit.paul@rentomojo.com>
2018-12-14 13:02:17 +0530

    "scaffolding lb4"

---
+++ test/acceptance/test-helper.ts
@@ -0,0 +1,24 @@
+import {Lb4BoilerplateApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const app = new Lb4BoilerplateApplication({
+    rest: givenHttpServerConfig(),
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: Lb4BoilerplateApplication;
+  client: Client;
+}
